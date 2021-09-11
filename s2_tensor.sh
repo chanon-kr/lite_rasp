01304b3c00efe5d95e7f8dfee7cebd2a65e6576e
Chanon Krittapholchai <chanonkr@scg.com>
2021-09-11 21:55:38 +0700

    Add tensorflow

--- s2_tensor.sh
+++ s2_tensor.sh
@@ -1,4 +1,4 @@
 git clone https://github.com/tensorflow/tensorflow.git tensorflow_src
 bazel build --config=elinux_aarch64 -c opt //tensorflow/lite/c:libtensorflowlite_c.so
 
-pip install https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow_cpu-2.6.0-cp37-cp37m-manylinux2010_x86_64.whl
+pip install https://storage.googleapis.com/tensorflow/linux/gpu/tensorflow_gpu-2.6.0-cp37-cp37m-manylinux2010_x86_64.whl
