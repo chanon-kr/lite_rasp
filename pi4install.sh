4294cd3aa431de40c2381175496ffc817a5b279a
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2021-11-15 13:22:33 +0700

    Add upgrade pip and Add dependency of OpenCV

--- pi4install.sh
+++ pi4install.sh
@@ -12,9 +12,16 @@ sudo apt-get dist-upgrade
 
 # Library
 sudo apt install python3-pip
-pip3 install opencv-python 
+pip3 install --upgrade pip
 pip3 install py-topping
 pip3 install google-cloud-storage
+
+# Open CV
+sudo apt-get -y install libjpeg-dev libtiff5-dev libjasper-dev libpng12-dev
+sudo apt-get -y install libavcodec-dev libavformat-dev libswscale-dev libv4l-dev
+sudo apt-get -y install libxvidcore-dev libx264-dev
+sudo apt-get -y install qt4-dev-tools libatlas-base-dev
+pip3 install opencv-python
 
 # TFLite Run Time for Linux
 echo "deb https://packages.cloud.google.com/apt coral-edgetpu-stable main" | sudo tee /etc/apt/sources.list.d/coral-edgetpu.list
