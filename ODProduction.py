57f571647b2f82e366e08f1e86c9807f9535f0bf
Chanon Krittapholchai <chanon.krittapholchai@gmail.com>
2021-12-22 11:14:34 +0700

    Add send error message in LINE when error

--- ODProduction.py
+++ ODProduction.py
@@ -10,6 +10,7 @@ import numpy as np
 import importlib.util
 from datetime import datetime, timedelta
 from py_topping.data_connection.gcp import da_tran_bucket
+from py_topping.general_use import lazy_LINE
 from glob import glob
 
 with open('config.json', 'rb') as f :
@@ -57,6 +58,7 @@ gcp_credential = config["gcp_credential"]
 save_size = float(config["save_size"])
 show_window = bool(config["show_window"])
 restart_limit = float(config["restart_hour"])
+error_line = config['error_line']
 # In[2]:
 
 
@@ -145,146 +147,150 @@ for i in ['tmp','tmp/Found','tmp/All'] :
 
 start_time = datetime.now()
 while(video.isOpened()):
-    # Acquire frame and resize to expected shape [1xHxWx3]
-    ret, frame = video.read()
-    now = datetime.now()
-    if not ret:
-        print('Reached the end of the video!')
-        break
+    try :
+        # Acquire frame and resize to expected shape [1xHxWx3]
+        ret, frame = video.read()
+        now = datetime.now()
+        if not ret:
+            print('Reached the end of the video!')
+            break
 
-    # Create Filename
-    filename = now.strftime('%Y%m%d%H%M%S')
-    # Create Check Point
-    now_minute = now.minute
-    temp_slot = (now.replace(minute = 0) + timedelta(minutes = int(now_minute/save_slot)*save_slot)).strftime('%Y%m%d%H%M')
+        # Create Filename
+        filename = now.strftime('%Y%m%d%H%M%S')
+        # Create Check Point
+        now_minute = now.minute
+        temp_slot = (now.replace(minute = 0) + timedelta(minutes = int(now_minute/save_slot)*save_slot)).strftime('%Y%m%d%H%M')
 
-    if now_slot != temp_slot : 
-        # Create Connection
-        print('Begin Save at',datetime.now())
-        gcs = da_tran_bucket(project_id = gcp_projectid
-                            , bucket_name = gcp_bucket
-                            , credential = gcp_credential )
+        if now_slot != temp_slot : 
+            # Create Connection
+            print('Begin Save at',datetime.now())
+            gcs = da_tran_bucket(project_id = gcp_projectid
+                                , bucket_name = gcp_bucket
+                                , credential = gcp_credential )
 
-        # Update Time
-        with open("tmp/lastupload.txt" , "w") as f :
-            f.write(now_slot)
-        gcs.upload(bucket_file = '{}/{}'.format(gcp_folder,"lastupload_begin.txt")
-                    ,local_file = "tmp/lastupload.txt")
-        os.remove("tmp/lastupload.txt")
-        # Save as Video (Save Space)
-        if len(fps_list) > 0 : 
-            fps = np.mean(fps_list)
-            print('Average FPS :',fps)
+            # Update Time
+            with open("tmp/lastupload.txt" , "w") as f :
+                f.write(now_slot)
+            gcs.upload(bucket_file = '{}/{}'.format(gcp_folder,"lastupload_begin.txt")
+                        ,local_file = "tmp/lastupload.txt")
+            os.remove("tmp/lastupload.txt")
+            # Save as Video (Save Space)
+            if len(fps_list) > 0 : 
+                fps = np.mean(fps_list)
+                print('Average FPS :',fps)
 
-            img_array_i = []
-            for i in glob('tmp/All/*.png'):
-                img_i = cv2.imread(i)
-                height_i, width_i, layers_i = img_i.shape
-                size_i = (width_i,height_i)
-                img_array_i.append(img_i)
-            out = cv2.VideoWriter('tmp/clip.avi',cv2.VideoWriter_fourcc(*'DIVX'), fps, size_i)   
-            for i in range(len(img_array_i)):
-                out.write(img_array_i[i])
-            out.release()
-            del img_array_i
-            gcs.upload(bucket_file = '{}/Clip/clip_{}.avi'.format(gcp_folder,now_slot)
-                        , local_file = 'tmp/clip.avi')
-            for i in glob('tmp/All/*.png') : os.remove(i)
-            os.remove('tmp/clip.avi')
-        # Save Found Picture
-        shutil.make_archive('tmp/Found', 'zip', 'tmp/Found')
-        gcs.upload(bucket_file = '{}/Found/Found_{}.zip'.format(gcp_folder,now_slot)
-                    , local_file = 'tmp/Found.zip')
-        # os.remove('tmp/Found.zip')
-        for i in glob('tmp/Found/*.png') : os.remove(i)
-        print('End Save at',datetime.now())
+                img_array_i = []
+                for i in glob('tmp/All/*.png'):
+                    img_i = cv2.imread(i)
+                    height_i, width_i, layers_i = img_i.shape
+                    size_i = (width_i,height_i)
+                    img_array_i.append(img_i)
+                out = cv2.VideoWriter('tmp/clip.avi',cv2.VideoWriter_fourcc(*'DIVX'), fps, size_i)   
+                for i in range(len(img_array_i)):
+                    out.write(img_array_i[i])
+                out.release()
+                del img_array_i
+                gcs.upload(bucket_file = '{}/Clip/clip_{}.avi'.format(gcp_folder,now_slot)
+                            , local_file = 'tmp/clip.avi')
+                for i in glob('tmp/All/*.png') : os.remove(i)
+                os.remove('tmp/clip.avi')
+            # Save Found Picture
+            shutil.make_archive('tmp/Found', 'zip', 'tmp/Found')
+            gcs.upload(bucket_file = '{}/Found/Found_{}.zip'.format(gcp_folder,now_slot)
+                        , local_file = 'tmp/Found.zip')
+            # os.remove('tmp/Found.zip')
+            for i in glob('tmp/Found/*.png') : os.remove(i)
+            print('End Save at',datetime.now())
 
-        # Update Time
-        with open("tmp/lastupload.txt" , "w") as f :
-            f.write(now_slot)
-        gcs.upload(bucket_file = '{}/{}'.format(gcp_folder,"lastupload_end.txt")
-                    ,local_file = "tmp/lastupload.txt")
-        os.remove("tmp/lastupload.txt")
-        # Break Time
-        cal_restart = round((now - start_time).total_seconds()/3600,2)
-        print('start @ :',start_time ,'\nCurrent Run {}/{} hours'.format(cal_restart,restart_limit))
-        if cal_restart > restart_limit : break
+            # Update Time
+            with open("tmp/lastupload.txt" , "w") as f :
+                f.write(now_slot)
+            gcs.upload(bucket_file = '{}/{}'.format(gcp_folder,"lastupload_end.txt")
+                        ,local_file = "tmp/lastupload.txt")
+            os.remove("tmp/lastupload.txt")
+            # Break Time
+            cal_restart = round((now - start_time).total_seconds()/3600,2)
+            print('start @ :',start_time ,'\nCurrent Run {}/{} hours'.format(cal_restart,restart_limit))
+            if cal_restart > restart_limit : break
 
-    now_slot = (now.replace(minute = 0) + timedelta(minutes = int(now_minute/save_slot)*save_slot)).strftime('%Y%m%d%H%M')
-    show = 0
-    
-    # Prep Image
-    frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-    frame_resized = frame_rgb[y1:y2,x1:x2]
-    frame_resized = cv2.resize(frame_resized, (width, height))
-    input_data = np.expand_dims(frame_resized, axis=0)
+        now_slot = (now.replace(minute = 0) + timedelta(minutes = int(now_minute/save_slot)*save_slot)).strftime('%Y%m%d%H%M')
+        show = 0
+        
+        # Prep Image
+        frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        frame_resized = frame_rgb[y1:y2,x1:x2]
+        frame_resized = cv2.resize(frame_resized, (width, height))
+        input_data = np.expand_dims(frame_resized, axis=0)
 
-    # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
-    if floating_model:
-        input_data = (np.float32(input_data) - input_mean) / input_std
+        # Normalize pixel values if using a floating model (i.e. if model is non-quantized)
+        if floating_model:
+            input_data = (np.float32(input_data) - input_mean) / input_std
 
-    # Perform the actual detection by running the model with the image as input
-    interpreter.set_tensor(input_details[0]['index'],input_data)
-    interpreter.invoke()
+        # Perform the actual detection by running the model with the image as input
+        interpreter.set_tensor(input_details[0]['index'],input_data)
+        interpreter.invoke()
 
-    # Solve Error (I think it's a bug) from some TFlite model
-    if 'lite' not in MODEL_NAME :
-        # Retrieve detection results : GCP AutoML
-        boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
-        classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
-        scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
-    else :
-        # For Some TFLite : efficientdet_lite0, 1, 2
-        boxes = interpreter.get_tensor(output_details[1]['index'])[0] # Bounding box coordinates of detected objects
-        classes = interpreter.get_tensor(output_details[3]['index'])[0] # Class index of detected objects
-        scores = interpreter.get_tensor(output_details[0]['index'])[0] # Confidence of detected objects
+        # Solve Error (I think it's a bug) from some TFlite model
+        if 'lite' not in MODEL_NAME :
+            # Retrieve detection results : GCP AutoML
+            boxes = interpreter.get_tensor(output_details[0]['index'])[0] # Bounding box coordinates of detected objects
+            classes = interpreter.get_tensor(output_details[1]['index'])[0] # Class index of detected objects
+            scores = interpreter.get_tensor(output_details[2]['index'])[0] # Confidence of detected objects
+        else :
+            # For Some TFLite : efficientdet_lite0, 1, 2
+            boxes = interpreter.get_tensor(output_details[1]['index'])[0] # Bounding box coordinates of detected objects
+            classes = interpreter.get_tensor(output_details[3]['index'])[0] # Class index of detected objects
+            scores = interpreter.get_tensor(output_details[0]['index'])[0] # Confidence of detected objects
 
-    # Loop over all detections and draw detection box if confidence is above minimum threshold
-    for i in range(len(scores)):
-        if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
-            # print(count_frame , 'found')
-            show += 1
-            # Get bounding box coordinates and draw box
-            # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
-            ymin = int(max(1,(boxes[i][0] * (y2-y1)))) + y1
-            xmin = int(max(1,(boxes[i][1] * (x2-x1)))) + x1
-            ymax = int(min((y2-y1),(boxes[i][2] * (y2-y1)))) + y1
-            xmax = int(min((x2-x1),(boxes[i][3] * (x2-x1)))) + x1
-            cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 4)
+        # Loop over all detections and draw detection box if confidence is above minimum threshold
+        for i in range(len(scores)):
+            if ((scores[i] > min_conf_threshold) and (scores[i] <= 1.0)):
+                # print(count_frame , 'found')
+                show += 1
+                # Get bounding box coordinates and draw box
+                # Interpreter can return coordinates that are outside of image dimensions, need to force them to be within image using max() and min()
+                ymin = int(max(1,(boxes[i][0] * (y2-y1)))) + y1
+                xmin = int(max(1,(boxes[i][1] * (x2-x1)))) + x1
+                ymax = int(min((y2-y1),(boxes[i][2] * (y2-y1)))) + y1
+                xmax = int(min((x2-x1),(boxes[i][3] * (x2-x1)))) + x1
+                cv2.rectangle(frame, (xmin,ymin), (xmax,ymax), (10, 255, 0), 4)
 
-            # Draw label
-            object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
-            label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
-            labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
-            label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
-            cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
-            cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
-    
-    # Add rectangle of Applied Area
-    cv2.rectangle(frame, (x2,y2), (x1,y1), (0, 255, 255), 2)
-    # All the results have been drawn on the frame, so it's time to display it.
-    if show_window : cv2.imshow('Object detector', frame)
-    
-    # To Save Space
-    frame = cv2.resize(frame, (int(imW*save_size), int(imH*save_size)))
-    # Save image
-    savename = 'tmp/All/at_{}.png'.format(filename) 
-    cv2.imwrite(savename, frame)
-    # Duplicate Picture with Label in other folder 
-    if show > 0 : 
-        savename = 'tmp/Found/at_{}.png'.format(filename)
+                # Draw label
+                object_name = labels[int(classes[i])] # Look up object name from "labels" array using class index
+                label = '%s: %d%%' % (object_name, int(scores[i]*100)) # Example: 'person: 72%'
+                labelSize, baseLine = cv2.getTextSize(label, cv2.FONT_HERSHEY_SIMPLEX, 0.7, 2) # Get font size
+                label_ymin = max(ymin, labelSize[1] + 10) # Make sure not to draw label too close to top of window
+                cv2.rectangle(frame, (xmin, label_ymin-labelSize[1]-10), (xmin+labelSize[0], label_ymin+baseLine-10), (255, 255, 255), cv2.FILLED) # Draw white box to put label text in
+                cv2.putText(frame, label, (xmin, label_ymin-7), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 0), 2) # Draw label text
+        
+        # Add rectangle of Applied Area
+        cv2.rectangle(frame, (x2,y2), (x1,y1), (0, 255, 255), 2)
+        # All the results have been drawn on the frame, so it's time to display it.
+        if show_window : cv2.imshow('Object detector', frame)
+        
+        # To Save Space
+        frame = cv2.resize(frame, (int(imW*save_size), int(imH*save_size)))
+        # Save image
+        savename = 'tmp/All/at_{}.png'.format(filename) 
         cv2.imwrite(savename, frame)
+        # Duplicate Picture with Label in other folder 
+        if show > 0 : 
+            savename = 'tmp/Found/at_{}.png'.format(filename)
+            cv2.imwrite(savename, frame)
 
-    # Save for cal fps
-    fps_cal = (datetime.now() - now).total_seconds()
-    if fps_cal == 0 : fps_cal = 60
-    else : fps_cal = 1/fps_cal
-    fps_list.append(fps_cal)
+        # Save for cal fps
+        fps_cal = (datetime.now() - now).total_seconds()
+        if fps_cal == 0 : fps_cal = 60
+        else : fps_cal = 1/fps_cal
+        fps_list.append(fps_cal)
 
-    # Press 'q' to quit
-    if cv2.waitKey(1) == ord('q'):
-        print('break')
-        break
+        # Press 'q' to quit
+        if cv2.waitKey(1) == ord('q'):
+            print('break')
+            break
+    except Exception as e :
+        line_sender = lazy_LINE(error_line)
+        line_sender.send(str(e))
 
 # Clean up
 video.release()
